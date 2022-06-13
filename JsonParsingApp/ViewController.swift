cac3650d79815aae95af9a1c0da360897681f57f
Pritam Dash <pritamdash2015@outlook.com>
2022-06-13 22:12:22 +0530

    TableViewUI complete.

--- JsonParsingApp/ViewController.swift
+++ JsonParsingApp/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource{
+    
+    @IBOutlet weak var downloadButton: UIButton!
+    @IBOutlet weak var downloadedDataTableView: UITableView!
+    
+    var dataArray = Array<String>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func downloadButtonClicked(_ sender: UIButton) {
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
 
